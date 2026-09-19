f0dab6105b99158cef69f28003409e605823bb4b
Jose <joselptecnico1989@gmail.com>
2026-09-18 20:29:11 -0500

    Disenos.jsx a disenos

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,6 +1,6 @@
 import Carrusel from "./Carrusel";
 import Servicios from "./Servicios";
-import Disenos from "./Disenos";
+import Disenos from "./disenos";
 import Galeria from "./Galeria";
 import Contacto from "./Contacto";
 
